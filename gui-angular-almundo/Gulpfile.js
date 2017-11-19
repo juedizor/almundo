1c8cab11fbfa17df8e47e718f9116f33c9272550
juedizor <juedizor@gmail.com>
2017-11-19 16:15:59 -0500

    se minimiza el codigo css y js para envio a produccion, se crean las tareas en gulp para asi crear el directorio dist, con los archivos necesarios para despliegue

--- gui-angular-almundo/Gulpfile.js
+++ gui-angular-almundo/Gulpfile.js
@@ -10,9 +10,52 @@ var opn = require('opn');
 var inject = require('gulp-inject');
 var wiredep = require('wiredep').stream;
 
+var gulpif = require('gulp-if'),
+  minifycss = require('gulp-minify-css'),
+  useref = require('gulp-useref'),
+  uglify = require('gulp-uglify');
+
+gulp.task('compress', function() {
+  gulp.src('./app/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify({
+      mangle: false
+    })))
+    .pipe(gulpif('*.css', minifycss()))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy', function() {
+  gulp.src('./app/index.html')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist'));
+  gulp.src('./app/views/**/hoteles/*')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist/views'));
+  gulp.src('./app/templates/**/app/*')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist/templates'));
+  gulp.src('./app/data/**/assets/**/icons/**/filters/*')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist/data/'));
+  gulp.src('./app/data/**/assets/**/images/*')
+    .pipe(useref())
+    .pipe(gulp.dest('./dist/data/'));
+});
+
+// production server
+gulp.task('prodServer', function() {
+  gulp.src('dist')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true,
+    }));
+});
+
 var options = {
-    bowerJson: require('./bower.json'),
-    directory: './app/lib/'
+  bowerJson: require('./bower.json'),
+  directory: './app/lib/'
 }
 
 // develop server
@@ -27,36 +70,43 @@ gulp.task('webserver', function() {
 
 // search error and show that
 gulp.task('jshint', function() {
-	return gulp.src('./app/scripts/**/*.js')
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('jshint-stylish'))
-		.pipe(jshint.reporter('fail'));
+  return gulp.src('./app/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 // Reload the web browser when there is change in html
 gulp.task('html', function() {
-	gulp.src('./app/**/*.html')
-	.pipe(connect.reload());
+  gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
 });
 
 // inject library installed with bower
-gulp.task('wiredep', function () {
-	return gulp.src('./app/index.html')
-		.pipe(wiredep(options))
-		.pipe(gulp.dest('./app'));
+gulp.task('wiredep', function() {
+  return gulp.src('./app/index.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./app'));
 });
 
 gulp.task('inject', function() {
-	var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css']);
-	return gulp.src('index.html', {cwd: './app'})
-	.pipe(inject(sources, {read: false, ignorePath: '/app'}))
-	.pipe(gulp.dest('./app'));
+  var sources = gulp.src(['./app/scripts/**/*.js', './app/stylesheets/**/*.css']);
+  return gulp.src('index.html', {
+      cwd: './app'
+    })
+    .pipe(inject(sources, {
+      read: false,
+      ignorePath: '/app'
+    }))
+    .pipe(gulp.dest('./app'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch(['./app/**/*.html'], ['html']);
-	gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
-	gulp.watch(['./bower.json'], ['wiredep']);
+  gulp.watch(['./app/**/*.html'], ['html']);
+  gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+  gulp.watch(['./bower.json'], ['wiredep']);
 });
 
-gulp.task('default', ['webserver',  'wiredep', 'watch', 'jshint', 'inject']);
+gulp.task('default', ['webserver', 'wiredep', 'watch', 'jshint', 'inject']);
+
+gulp.task('build', ['compress', 'copy']);
